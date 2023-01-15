77c88e423f30c4dea396ed7a9073cf21816c88a7
M1ngXU <max.obreiter@gmail.com>
2023-01-15 17:10:11 +0100

    a3b

--- bwinf41-2/a1/Quelltext/bin.rs
+++ bwinf41-2/a1/Quelltext/bin.rs
@@ -1,66 +1,232 @@
-use std::f64::consts::PI;
+use bit_vec::BitVec;
+use image::{ImageBuffer, Rgb};
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+use show_image::create_window;
+use std::{
+    collections::{BinaryHeap, HashMap, HashSet},
+    fmt::{Display, Formatter},
+    hash::Hash,
+    io::Write,
+    ops::Deref,
+};
 
-struct Location {
-    x: f64,
-    y: f64,
+use aufgaben_helfer::loese_aufgabe;
+use imageproc::drawing::{draw_hollow_circle_mut, draw_line_segment_mut};
+use itertools::Itertools;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+struct Ort {
+    x: i64,
+    y: i64,
+    i: usize,
 }
-
-struct Turn {
-    from: usize,
-    to: usize,
-    angle: f64,
+impl Display for Ort {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}.{:0<6}|{}.{:0<6}",
+            self.x / 1_000_000,
+            self.x % 1_000_000,
+            self.y / 1_000_000,
+            self.y % 1_000_000
+        )
+    }
 }
-
-fn calculate_route(locations: &[Location], turns: &[Turn]) -> Vec<usize> {
-    let mut route = Vec::new();
-    let mut visited = vec![false; locations.len()];
-
-    // Start at an arbitrary location
-    route.push(0);
-    visited[0] = true;
-
-    while route.len() < locations.len() {
-        let current = *route.last().unwrap();
-
-        let mut next = None;
-        let mut next_angle = std::f64::INFINITY;
-
-        // Find the next location to visit that has the smallest angle
-        for turn in turns {
-            if turn.from == current && !visited[turn.to] && turn.angle <= next_angle {
-                next = Some(turn.to);
-                next_angle = turn.angle;
-            }
-        }
-
-        if let Some(next) = next {
-            route.push(next);
-            visited[next] = true;
-        } else {
-            // If there are no more locations to visit, terminate the loop
-            break;
-        }
+impl Ort {
+    fn moegliche_abbiegung(&self, von: &Self, zu: &Self) -> bool {
+        let a = (self.x - von.x, self.y - von.y);
+        let b = (zu.x - self.x, zu.y - self.y);
+        let skalarprodukt = a.0 * b.0 + a.1 * b.1;
+        skalarprodukt >= 0
     }
 
-    route
+    fn nachfolger_orte(&self, von: &Self, offene_orte: &Vec<Self>) -> Vec<Self> {
+        offene_orte
+            .iter()
+            .filter(|zu| self.moegliche_abbiegung(von, zu))
+            .copied()
+            .collect()
+    }
+
+    fn kosten_zu(&self, zu: &Self) -> u64 {
+        ((self.x - zu.x).pow(2) + (self.y - zu.y).pow(2)) as u64
+    }
 }
 
-fn main() {
-    let locations = vec![
-        Location { x: 0.0, y: 0.0 },
-        Location { x: 1.0, y: 0.0 },
-        Location { x: 1.0, y: 1.0 },
-        Location { x: 0.0, y: 1.0 },
-    ];
+#[derive(Clone, Hash, Debug, PartialEq, Eq)]
+struct OrtNachKosten {
+    besucht: BitVec,
+    ort: Kante,
+}
+#[derive(Clone, Hash, Debug)]
+struct Wrapper(OrtNachKosten, u64, Vec<Kante>);
+impl Deref for Wrapper {
+    type Target = OrtNachKosten;
 
-    let turns = vec![
-        Turn { from: 0, to: 1, angle: 0.0 },
-        Turn { from: 1, to: 2, angle: PI / 2.0 },
-        Turn { from: 2, to: 3, angle: PI },
-        Turn { from: 3, to: 0, angle: 3.0 * PI / 2.0 },
-    ];
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl PartialEq for Wrapper {
+    fn eq(&self, other: &Self) -> bool {
+        self.1 == other.1
+    }
+}
+impl Eq for Wrapper {}
+impl PartialOrd for Wrapper {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for Wrapper {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.1.cmp(&other.1).reverse()
+    }
+}
 
-    let route = calculate_route(&locations, &turns);
+#[derive(Clone, Copy, Hash, Debug, PartialEq, Eq)]
+struct Kante {
+    von: Ort,
+    zu: Ort,
+}
 
-    println!("Route: {:?}", route);
+pub fn a1(eingabe: String) {
+    let orte = eingabe
+        .trim_start_matches('\u{feff}') // manchmal gibt es ein BOM am Anfang eines Strings
+        .split_ascii_whitespace()
+        .map(|n| n.replacen('.', "", 1).parse::<i64>().expect("Keine Zahl!"))
+        .tuples()
+        .enumerate()
+        .map(|(i, (x, y))| Ort { x, y, i })
+        .collect_vec();
+    let kanten = orte
+        .iter()
+        .copied()
+        .cartesian_product(orte.clone())
+        .cartesian_product(orte.clone())
+        .map(|((a, b), c)| (a, b, c))
+        .filter(|(a, b, c)| a != b && a != c && b != c)
+        .filter(|(a, b, c)| b.moegliche_abbiegung(a, c))
+        .map(|(a, b, c)| (Kante { von: a, zu: b }, Kante { von: b, zu: c }))
+        .into_group_map();
+    let bester_pfad = kanten
+        .clone()
+        .into_par_iter()
+        .find_map_any(|(start, nachfolger)| {
+            let mut warteschlange = BinaryHeap::<Wrapper>::new();
+            let mut gesehen = HashSet::<OrtNachKosten>::new();
+            let mut besucht = BitVec::from_elem(orte.len(), false);
+            besucht.set(start.zu.i, true);
+            warteschlange.push(Wrapper(
+                OrtNachKosten {
+                    besucht,
+                    ort: start,
+                },
+                start.von.kosten_zu(&start.zu),
+                vec![start],
+            ));
+
+            while let Some(naechster) = warteschlange.pop() {
+                if gesehen.contains(&naechster) {
+                    continue;
+                } else {
+                    gesehen.insert(naechster.clone().0);
+                }
+                if naechster.besucht.all() {
+                    assert_eq!(naechster.2[0].von, naechster.2[orte.len() - 1].zu);
+                    return Some(naechster);
+                }
+                for naechster_ort in kanten
+                    .get(&naechster.ort)
+                    .into_iter()
+                    .flatten()
+                    .filter(|k| {
+                        !naechster.besucht[k.zu.i]
+                            && (k.zu != start.von
+                                || naechster.besucht.iter().filter(|x| !*x).count() == 1)
+                    })
+                {
+                    let mut besucht = naechster.besucht.clone();
+                    besucht.set(naechster_ort.zu.i, true);
+                    let mut pfad = naechster.2.clone();
+                    pfad.push(*naechster_ort);
+                    warteschlange.push(Wrapper(
+                        OrtNachKosten {
+                            ort: *naechster_ort,
+                            besucht,
+                        },
+                        naechster.1 + naechster.ort.zu.kosten_zu(&naechster_ort.von),
+                        pfad,
+                    ))
+                }
+            }
+            None
+        })
+        //.max_by_key(|n| n.1)
+        .unwrap();
+    let scale = 500_000;
+    let min_x = orte.iter().map(|o| o.x).min().unwrap_or_default() / scale;
+    let min_y = orte.iter().map(|o| o.y).min().unwrap_or_default() / scale;
+    let max_x = orte.iter().map(|o| o.x).max().unwrap_or_default() / scale;
+    let max_y = orte.iter().map(|o| o.y).max().unwrap_or_default() / scale;
+    let offset_x = -min_x + 100;
+    let offset_y = -min_y + 100;
+    let width = (max_x + offset_x + 100) as u32;
+    let height = (max_y + offset_y + 100) as u32;
+
+    let mut img: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(width, height);
+    for pixel in img.pixels_mut() {
+        (*pixel).0 = [255, 255, 255];
+    }
+
+    for kante in &bester_pfad.2 {
+        draw_line_segment_mut(
+            &mut img,
+            (
+                (kante.von.x) as f32 / scale as f32 + offset_x as f32,
+                (kante.von.y) as f32 / scale as f32 + offset_y as f32,
+            ),
+            (
+                (kante.zu.x) as f32 / scale as f32 + offset_x as f32,
+                (kante.zu.y) as f32 / scale as f32 + offset_y as f32,
+            ),
+            Rgb([0, 0, 0]),
+        );
+        draw_hollow_circle_mut(
+            &mut img,
+            (
+                (kante.von.x / scale + offset_x) as i32,
+                (kante.von.y / scale + offset_y) as i32,
+            ),
+            2,
+            Rgb([0, 0, 255]),
+        );
+        draw_hollow_circle_mut(
+            &mut img,
+            (
+                (kante.zu.x / scale + offset_x) as i32,
+                (kante.zu.y / scale + offset_y) as i32,
+            ),
+            2,
+            Rgb([0, 0, 255]),
+        );
+    }
+    draw_hollow_circle_mut(
+        &mut img,
+        (
+            (bester_pfad.2[0].von.x / scale + offset_x) as i32,
+            (bester_pfad.2[0].von.y / scale + offset_y) as i32,
+        ),
+        5,
+        Rgb([255, 0, 0]),
+    );
+    let window = create_window("image", Default::default()).unwrap();
+    window.set_image("image", img).unwrap();
+    window.wait_until_destroyed().unwrap();
+}
+
+#[show_image::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    loese_aufgabe(a1);
+    Ok(())
 }
--- bwinf41-2/a3/Quelltext/bin.rs
+++ bwinf41-2/a3/Quelltext/bin.rs
@@ -1,7 +1,12 @@
-use aufgaben_helfer::loese_aufgabe;
+use itertools::Itertools;
 use tinyvec::ArrayVec;
 
-use std::{collections::HashMap, fmt::Debug};
+use std::{
+    collections::HashMap,
+    fmt::Debug,
+    sync::{Arc},
+    time::Instant,
+};
 
 const MAX_STAPEL_GROESSE: usize = 16;
 
@@ -29,19 +34,18 @@ impl From<&str> for Stapel {
 }
 impl Stapel {
     // TODO inefficient
-    fn wenden_und_essen(&self, index: usize, normalisieren: bool) -> Self {
-        assert!(!self.stapel.is_empty());
-        assert!(index < self.stapel.len());
+    fn wenden_und_essen(&self, index: u8, normalisieren: bool) -> Self {
+        assert!(self.stapel.len() > index as usize);
         let mut neuer_stapel = ArrayVec::new();
-        let gegessen = self.stapel[index];
+        let gegessen = self.stapel[index as usize];
         for i in 0..index {
-            let mut tmp = self.stapel[i];
+            let mut tmp = self.stapel[i as usize];
             if normalisieren && tmp > gegessen {
                 tmp -= 1;
             }
             neuer_stapel.push(tmp);
         }
-        for i in 0..self.stapel.len() - index - 1 {
+        for i in 0..self.stapel.len() - index as usize - 1 {
             let mut tmp = self.stapel[self.stapel.len() - 1 - i];
             if normalisieren && tmp > gegessen {
                 tmp -= 1;
@@ -75,64 +79,222 @@ impl Stapel {
     }
 }
 
-fn stapel_durchprobieren(
-    gesehen: &mut HashMap<Stapel, ArrayVec<[u8; MAX_STAPEL_GROESSE]>>,
+// fn stapel_durchprobieren(
+//     gesehen: &mut HashMap<Stapel, ArrayVec<[u8; MAX_STAPEL_GROESSE]>>,
+//     stapel: Stapel,
+// ) -> ArrayVec<[u8; MAX_STAPEL_GROESSE]> {
+//     if let Some(status) = gesehen.get(&stapel) {
+//         *status
+//     } else {
+//         let beste_operationen = if stapel.is_sorted() {
+//             ArrayVec::new()
+//         } else {
+//             let mut beste_operationen: Option<ArrayVec<[u8; MAX_STAPEL_GROESSE]>> = None;
+//             // test all states & pick the best one
+//             for i in 0..stapel.stapel.len() {
+//                 let neuer_stapel = stapel.wenden_und_essen(i, true);
+//                 let mut neuer_status = stapel_durchprobieren(gesehen, neuer_stapel);
+//                 if beste_operationen
+//                     .map(|b| b.len())
+//                     .filter(|l| *l <= neuer_status.len() + 1)
+//                     .is_none()
+//                 {
+//                     neuer_status.push(i as u8);
+//                     beste_operationen = Some(neuer_status);
+//                 }
+//             }
+//             beste_operationen.unwrap()
+//         };
+//         gesehen.insert(stapel, beste_operationen);
+//         beste_operationen
+//     }
+// }
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+struct Flip(u8);
+impl Flip {
+    fn new(v: Option<u8>) -> Self {
+        Self(v.unwrap_or(u8::MAX))
+    }
+
+    fn as_option(&self) -> Option<u8> {
+        (self.0 < u8::MAX).then_some(self.0)
+    }
+}
+impl From<Option<u8>> for Flip {
+    fn from(value: Option<u8>) -> Self {
+        Self::new(value)
+    }
+}
+impl From<Flip> for Option<u8> {
+    fn from(value: Flip) -> Self {
+        value.as_option()
+    }
+}
+
+fn stapel_durchprobieren2(
+    gesehen: &HashMap<Stapel, (usize, Flip)>,
     stapel: Stapel,
-) -> ArrayVec<[u8; MAX_STAPEL_GROESSE]> {
+) -> (usize, Flip) {
     if let Some(status) = gesehen.get(&stapel) {
         *status
     } else {
         let beste_operationen = if stapel.is_sorted() {
-            ArrayVec::new()
+            (0, None.into())
         } else {
-            let mut beste_operationen: Option<ArrayVec<[u8; MAX_STAPEL_GROESSE]>> = None;
+            let mut beste_operationen: Option<(usize, Flip)> = None;
             // test all states & pick the best one
-            for i in 0..stapel.stapel.len() {
+            for i in 0..stapel.stapel.len() as u8 {
                 let neuer_stapel = stapel.wenden_und_essen(i, true);
-                let mut neuer_status = stapel_durchprobieren(gesehen, neuer_stapel);
-                if beste_operationen
-                    .map(|b| b.len())
-                    .filter(|l| *l <= neuer_status.len() + 1)
-                    .is_none()
-                {
-                    neuer_status.push(i as u8);
-                    beste_operationen = Some(neuer_status);
+                let (anzahl, _) = stapel_durchprobieren2(gesehen, neuer_stapel);
+                if beste_operationen.filter(|(l, _)| l <= &anzahl).is_none() {
+                    beste_operationen = Some((anzahl + 1, Some(i).into()));
                 }
             }
             beste_operationen.unwrap()
         };
-        gesehen.insert(stapel, beste_operationen);
         beste_operationen
     }
 }
 
-fn print(mut stapel: Stapel, wende_und_ess_operationen: &ArrayVec<[u8; MAX_STAPEL_GROESSE]>) {
-    println!("A(s) = {}", wende_und_ess_operationen.len());
+fn print(mut stapel: Stapel, gesehen: &HashMap<Stapel, (usize, Flip)>) {
     println!("Anfangsstapel:");
     let anfangs_groesse = stapel.stapel.len();
     stapel.print(anfangs_groesse);
     println!();
 
-    for wende_und_ess_operation in wende_und_ess_operationen.into_iter().rev() {
+    let mut stapel_to_print = stapel;
+
+    while let Some(wende_und_ess_operation) = gesehen.get(&stapel).and_then(|(_, f)| f.as_option())
+    {
         println!("Ess-und-Wende-Operation bei: {wende_und_ess_operation}");
         println!();
-        stapel = stapel.wenden_und_essen(*wende_und_ess_operation as _, false);
-        stapel.print(anfangs_groesse);
+        stapel = stapel.wenden_und_essen(wende_und_ess_operation, true);
+        stapel_to_print = stapel_to_print.wenden_und_essen(wende_und_ess_operation, false);
+        stapel_to_print.print(anfangs_groesse);
         println!();
     }
 }
 
-pub fn a3(eingabe: String) {
-    let anfangs_stapel = Stapel::from(eingabe.as_str());
+// pub fn a3_a(eingabe: String) {
+//     let anfangs_stapel = Stapel::from(eingabe.as_str());
+//     let mut gesehen = HashMap::new();
+//     print(
+//         anfangs_stapel,
+//         &stapel_durchprobieren(&mut gesehen, anfangs_stapel),
+//     );
+// }
+
+// copied from unstable u64::div_ceil
+#[inline]
+pub const fn div_ceil(a: u64, b: u64) -> u64 {
+    let d = a / b;
+    let r = a % b;
+    if r > 0 && b > 0 {
+        d + 1
+    } else {
+        d
+    }
+}
+
+macro_rules! exec_time {
+    () => {
+        $crate::eprintln!("[{}:{}]", $crate::file!(), $crate::line!())
+    };
+    ($val:expr $(,)?) => {{
+        let start = std::time::Instant::now();
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                eprintln!("[{}:{}] {} = {:?}",
+                    file!(), line!(), stringify!($val), start.elapsed());
+                tmp
+            }
+        }
+    }};
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
+
+pub fn a3_b(limit: u8) {
+    let start = Instant::now();
+
     let mut gesehen = HashMap::new();
-    print(
-        anfangs_stapel,
-        &stapel_durchprobieren(&mut gesehen, anfangs_stapel),
-    );
+    let mut worst_cases = Vec::new();
+    for i in 1..=limit {
+        let mut worst_case: Option<(Stapel, usize, Flip)> = None;
+        let factorial = (1..=i as u64).product1().unwrap_or(1_u64);
+        let thread_count = std::thread::available_parallelism().unwrap().get();
+        let chunk_size = div_ceil(factorial, thread_count as u64) as usize;
+        let permutations = (1..=i)
+            .permutations(i as usize)
+            .chunks(chunk_size)
+            .into_iter()
+            .map(|c| c.collect_vec())
+            .collect_vec();
+        let mut gesehen_neu = HashMap::new();
+        let gesehen_arc = Arc::new(gesehen);
+        for handle in permutations.into_iter().map(|chunk| {
+            let gesehen_clone = gesehen_arc.clone();
+            std::thread::spawn(move || {
+                let gesehen = gesehen_clone;
+                let mut worst_case: Option<(Stapel, usize, Flip)> = None;
+                let mut gesehen_neu = HashMap::new();
+                for s in chunk {
+                    let stapel = Stapel {
+                        stapel: ArrayVec::from_iter(s),
+                    };
+                    let (laenge, index) = stapel_durchprobieren2(&gesehen, stapel);
+                    gesehen_neu.insert(stapel, (laenge, index));
+                    if worst_case.filter(|(_, l, _)| l >= &laenge).is_none() {
+                        worst_case = Some((stapel, laenge, index));
+                    }
+                }
+                (worst_case, gesehen_neu)
+            })
+        }) {
+            let (wc, gesehen_n) = handle.join().unwrap();
+            gesehen_neu.extend(gesehen_n);
+            if let Some((stapel, laenge, index)) = wc {
+                if worst_case.filter(|(_, l, _)| l >= &laenge).is_none() {
+                    worst_case = Some((stapel, laenge, index));
+                }
+            }
+        }
+        worst_cases.push(worst_case);
+        gesehen = Arc::try_unwrap(gesehen_arc).unwrap();
+        gesehen.extend(gesehen_neu);
+
+        if let Some((stapel, laenge, index)) = worst_case {
+            println!("P({i}) = {laenge}");
+            println!();
+            println!("Beispiel:");
+            print(stapel, &gesehen);
+            println!();
+        }
+    }
+
+    println!("{:^5} | {:^5}", "n", "P(n)");
+    println!("{:-^5}-+-{:-^5}", "", "");
+    for (n, pn) in worst_cases
+        .into_iter()
+        .enumerate()
+        .filter_map(|(n, w)| w.map(|(_, l, _)| (n + 1, l)))
+    {
+        println!("{n:^5} | {pn:^5}");
+    }
+
+    println!();
+    println!("Ausführungsdauer: {}ms", start.elapsed().as_millis());
 }
 
 fn main() {
-    loese_aufgabe(a3);
+    match std::env::args().nth(1).and_then(|n| n.parse::<u8>().ok()) {
+        Some(limit) if std::env::args().count() == 2 => a3_b(limit),
+        _ => todo!(), //loese_aufgabe(a3_a),
+    }
 }
 
 #[cfg(test)]
